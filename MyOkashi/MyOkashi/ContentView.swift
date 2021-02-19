32699a95d4732f03284a69e19a20043050c0cc35
Shusaku-K <77326356+Shusaku-K@users.noreply.github.com>
2021-02-19 16:44:13 +0900

    Add files via upload

---
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 金城秀作 on 2021/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var okashiDataList = OkashiData()
+    
+    
+    @State var inputText = ""
+    
+    @State var showSafari = false
+    
+    
+    var body: some View {
+
+        VStack {
+            TextField("キーワードを入力してください", text: $inputText, onCommit: {
+                okashiDataList.searchOkashi(keyword:  inputText)
+            })
+            
+            .padding()
+            
+            List(okashiDataList.okashiList) { okashi in
+                
+                
+                Button(action: {
+                    showSafari.toggle()
+                }) {
+                    
+                    HStack {
+                        Image(uiImage: okashi.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                        
+                        
+                        
+                        Text(okashi.name)
+                    }
+                }
+                
+                .sheet(isPresented: self.$showSafari, content: {
+                    SafariView(url: okashi.link)
+                        .edgesIgnoringSafeArea(.bottom)
+                })
+            }
+            
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashi/MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 金城秀作 on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
